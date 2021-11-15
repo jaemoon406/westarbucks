4840433bada24894103023a5ca9d21a0205c059c
jaemoon <jaemoon406@gmail.com>
2021-11-15 14:34:36 +0900

    [add]Westarbucks

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+
+SECRET_KEY = 'django-insecure-o-t*i=t%7g7o2__#$6d54b8vax2b3*o1+wy=!^8=x^z^w29ap5'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Westarbucks',
+        'USER': 'root',
+        'PASSWORD': 'Dlwoans12!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+				'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
